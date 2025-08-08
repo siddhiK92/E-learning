879f022ab12de908877fb64673baa7b4b34d34ac
siddhik92 <siddhikawade92@gmail.com>
2025-08-08 19:24:01 +0530

    Course Structure

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -1,11 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { AddContext } from '../../context/AddContext';
+import { useParams } from 'react-router-dom';
+import { assets } from '../../assets/assets';
 
 const Player = () => {
-  return (
-    <div>
-      <h1>Player page</h1>
-    </div>
-  )
-}
 
-export default Player
+  const {enrolledCourses, calculateChapterTime} = useContext(addContext);
+  const {courseId} = useParams();
+  const [courseData, setCourseData] = useState(null)
+  const [openSections, setOpenSections] = useState({})
+  const [playerData, setPlayerData] = useState(null)
+
+  const getCourseData = ()=>{
+    enrolledCourses.map((course)=>{
+      if(course._id === courseId){
+        setCourseData(course)
+      }
+    })
+  }
+
+  const toggleSection = (index) => {
+    setOpenSections((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
+  useEffect(()=>{
+    getCourseData()
+  }, [])
+
+  return (
+    <>
+      <div className='p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
+        
+        {/* left column */}
+        <div className='text-gray-800'>
+          <h2 className='text-xl font-semibold'>Course Structure</h2>
+
+
+          <div className="pt-5">
+            {courseData && courseData.courseContent.map((chapter, index) => (
+              <div
+                key={index}
+                className="border border-gray-300 bg-white mb-2 rounded"
+              >
+                <div
+                  className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                  onClick={() => toggleSection(index)}
+                >
+                  <div className="flex items-center gap-2">
+                    <img
+                      className={`transform transition-transform ${
+                        openSections[index] ? "rotate-180" : ""
+                      }`}
+                      src={assets.down_arrow_icon}
+                      alt="arrow icon"
+                    />
+                    <p className="font-medium md:text-base text-sm">
+                      {chapter.chapterTitle}
+                    </p>
+                  </div>
+                  <p className="text-sm md:text-default">
+                    {chapter.chapterContent.length} lectures -{" "}
+                    {calculateChapterTime(chapter)}
+                  </p>
+                </div>
+
+                {/* Expandable lecture list */}
+                <div
+                  className={`overflow-hidden transition-all duration-300 ${
+                    openSections[index] ? "max-h-96" : "max-h-0"
+                  }`}
+                >
+                  <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                    {chapter.chapterContent.map((lecture, i) => (
+                      <li key={i} className="flex items-start gap-2 py-1">
+                        <img
+                          src={false ? assets.blue_tick_icon : assets.play_icon}
+                          alt="play icon"
+                          className="w-4 h-4 mt-1"
+                        />
+                        <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                          <p>{lecture.lectureTitle}</p>
+                          <div className="flex gap-2">
+                            {lecture.isPreviewFree && (
+                              <p
+                                onClick={() =>
+                                  setPlayerData({
+                                    videoId: lecture.lectureUrl.split('/').pop(),
+                                  })
+                                }
+                                className="text-blue-500 cursor-pointer"
+                              >
+                                Preview
+                              </p>
+                            )}
+                            <p className="humanize">
+                              {lecture.lectureDuration
+                                ? humanizeDuration(
+                                    lecture.lectureDuration * 60 * 1000,
+                                    {
+                                      units: ["h", "m"],
+                                      round: true,
+                                      largest: 2,
+                                      conjunction: '',
+                                      serialComma: false,
+                                      spacer: " ",
+                                      delimiter: ", ",
+                                    }
+                                  )
+                                : "Duration not available"}
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* right column */}
+        <div></div>
+
+      </div>
+    </>
+  );
+};
+
+export default Player;
