59838a979de9c6a3254380c6be4b6a55a76c37a9
siddhiK92 <siddhikawade92@gmail.com>
2025-09-17 10:03:15 +0530

    Delete client directory

--- client/postcss.config.js
+++
@@ -1,7 +0,0 @@
-
-import tailwindcss from 'tailwindcss'; // ✅ CORRECT
-import autoprefixer from 'autoprefixer'; // ✅
-
-export default {
-  plugins: [tailwindcss, autoprefixer], // ✅ ARRAY, not object
-};
